939fe2806e2d55e12e419bc98b1facb96749981c
Andre Antolis <andreantolis11@gmail.com>
2022-10-03 11:44:24 +0700

    add project

---
+++ src/main/java/com/simasforum/SimasForum/SimasForumApplication.java
@@ -0,0 +1,13 @@
+package com.simasforum.SimasForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimasForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimasForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simasforum/SimasForum/SimasForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simasforum.SimasForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimasForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
