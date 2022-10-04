11324a2f276e79df5398812e10264dc25b4d575d
unknown <rinaldikarokaro@gmail.com>
2022-10-04 15:44:20 +0700

    update

--- src/main/java/com/simasforum/SimasForum/service/ThreadService.java
+++ src/main/java/com/simasforum/SimasForum/service/ThreadService.java
@@ -1,6 +1,7 @@
 package com.simasforum.SimasForum.service;
 
 import com.simasforum.SimasForum.model.Thread;
+import com.simasforum.SimasForum.model.User;
 import com.simasforum.SimasForum.repository.ThreadRepository;
 import liquibase.pro.packaged.T;
 import org.slf4j.Logger;
@@ -8,7 +9,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -20,13 +26,17 @@ public class ThreadService {
     private static final String TODO_LIST_DOES_NOT_EXIST_FMT = "Thread(id=%d) does not exist";
 
     private ThreadRepository threadRepository;
+    private ThreadService threadService;
 
     @Autowired
     public void setThreadRepository(ThreadRepository threadRepository) {
         this.threadRepository = threadRepository;
     }
 
-    public Thread getTodoListById(Long id) throws NoSuchElementException {
+    public void setThreadService(ThreadService threadService) {
+        this.threadService = threadService;
+    }
+    public Thread getThreadtById(Long id) throws NoSuchElementException {
         Optional<Thread> result = threadRepository.findById(id);
 
         return result.get();
@@ -42,6 +52,18 @@ public class ThreadService {
         Optional<Thread> result = threadRepository.findById(id);
 
         return result;
+    }
+
+    @GetMapping("/thread/add")
+    public String newThread(Model model, User user) {
+        model.addAttribute("user_id", 1L);
+        return "/add_thread";
+    }
+
+    @PostMapping("/thread/add")
+    public String newThread(@RequestParam("title") String title, @RequestParam("content") String content){
+        threadService.addThread(new Thread(title, content, 0, 0, LocalDate.now()));
+        return "/my_thread";
     }
 
     public List<Thread> sortByDate(){
