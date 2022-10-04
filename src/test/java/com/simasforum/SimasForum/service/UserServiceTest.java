701706861a436e3eefef755bf3a6a8e3fbb08482
Calvin Hartanto <calvinhonggare@gmail.com>
2022-10-04 16:26:23 +0700

    add user login and update register

--- src/test/java/com/simasforum/SimasForum/service/UserServiceTest.java
+++ src/test/java/com/simasforum/SimasForum/service/UserServiceTest.java
@@ -1,8 +1,15 @@
 package com.simasforum.SimasForum.service;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,15 +29,35 @@ class UserServiceTest {
     @MockBean
     private UserRepository userRepository;
 
-
     @Test
     @DisplayName("Given a new user, addUser should save the user")
     void addUser_ok() {
         User user = new User("testuser","password","email@gmail.com");
-        when(userRepository.save(any(User.class))).thenReturn(user);
-
         User registeredUser = userService.addUser(user);
-
-        assertFalse(user.getName().isEmpty());
+        verify(userRepository, times(1)).save(any(User.class));
     }
+    
+    @Test
+    @DisplayName("Given a new user, addUser should save the user")
+    void getUserById_ok() {
+        Long id = 1L;
+        Optional<User> mockUser= Optional.of(new User("name","email@email.com","password"));
+        when(userRepository.findById(anyLong())).thenReturn(mockUser);
+        User registeredUser = userService.getUserById(id);
+        verify(userRepository, times(1)).findById(any(Long.class));
+        assertTrue(registeredUser.getName().equals("name"));
+    }
+    
+    @Test
+    @DisplayName("Given a new user, addUser should save the user")
+    void getUserEmail_ok() {
+    	String email = "email@email.com";
+    	Optional<User> mockUser= Optional.of(new User("name",email,"password"));
+    	when(userRepository.findByEmail(anyString())).thenReturn(mockUser);
+    	User registeredUser = userService.getUserByEmail(email);
+    	verify(userRepository, times(1)).findByEmail(any(String.class));
+    	assertTrue(registeredUser.getEmail().equals(email));
+    }
+    
+    
 }
