5c8ae13eaf4ff7a9a921140e85ad57ba80d5f1a1
fadhluli <fadhlulirsyad16@gmail.com>
2022-10-05 07:32:36 +0700

    add new thread test

--- src/test/java/com/simasforum/SimasForum/controller/ThreadControllerTest.java
+++ src/test/java/com/simasforum/SimasForum/controller/ThreadControllerTest.java
@@ -2,15 +2,19 @@ package com.simasforum.SimasForum.controller;
 
 import com.simasforum.SimasForum.model.Thread;
 import com.simasforum.SimasForum.service.ThreadService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.TEXT_HTML;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ThreadController.class)
 public class ThreadControllerTest {
@@ -27,6 +31,17 @@ public class ThreadControllerTest {
 
         mockMvc.perform(post("/thread").param("item_text", "text")).andExpectAll(
                 status().is3xxRedirection()
+        );
+    }
+
+    @Test
+    @DisplayName("HTTP GET '/thread/add' show add_thread.html")
+    void showList_resolvesToIndex() throws Exception {
+        mockMvc.perform(get("/thread/add")).andExpectAll(
+                status().isOk(),
+                content().contentTypeCompatibleWith(TEXT_HTML),
+                content().encoding(UTF_8),
+                view().name("add_thread")
         );
     }
 }
