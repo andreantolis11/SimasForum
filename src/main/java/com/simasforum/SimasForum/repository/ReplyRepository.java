0e0a94d8e069060bec067521cfff82ea9c0d1c91
unknown <rinaldikarokaro@gmail.com>
2022-10-10 11:25:47 +0700

    reply model,repository,sql

---
+++ src/main/java/com/simasforum/SimasForum/repository/ReplyRepository.java
@@ -0,0 +1,7 @@
+package com.simasforum.SimasForum.repository;
+
+import com.simasforum.SimasForum.model.Reply;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReplyRepository extends CrudRepository<Reply,Long> {
+}
