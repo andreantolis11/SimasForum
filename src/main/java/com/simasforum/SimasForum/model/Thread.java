c326d961ad9e69e59036431495de5c801ec6b5f2
Faustine Ben Axel Ompusunggu <faustinebenaxel08@gmail.com>
2022-10-17 10:52:27 +0700

    created report model, repo & sql

--- src/main/java/com/simasforum/SimasForum/model/Thread.java
+++ src/main/java/com/simasforum/SimasForum/model/Thread.java
@@ -20,6 +20,9 @@ public class Thread {
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "threadId")
     private List<Reply> reply;
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "threadId")
+    private List<Report> report;
     @Column(nullable = false)
     private String title;
     @Column(nullable = false)
