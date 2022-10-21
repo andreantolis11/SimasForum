b06d040d5c089ea86726568628ea29c91d75e854
fadhluli <fadhlulirsyad16@gmail.com>
2022-10-21 14:06:48 +0700

    100% code coverage

--- src/main/java/com/simasforum/SimasForum/SimasForumApplication.java
+++ src/main/java/com/simasforum/SimasForum/SimasForumApplication.java
@@ -1,10 +1,12 @@
 package com.simasforum.SimasForum;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@Generated
 public class SimasForumApplication {
 
 	public static void main(String[] args) {
